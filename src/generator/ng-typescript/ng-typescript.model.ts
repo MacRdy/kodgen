1f9c5f6c0f9020111c5194fc5b3258ab3bcc75b9
Anton Shibaev <anton@shibaev.io>
2022-10-28 01:08:13 +0300

    enum generation

---
+++ src/generator/ng-typescript/ng-typescript.model.ts
@@ -0,0 +1,10 @@
+export interface INgtsEnumEntry<T = unknown> {
+	name: string;
+	value: T;
+}
+
+export interface INgtsEnum {
+	name: string;
+	isStringlyTyped: boolean;
+	entries: INgtsEnumEntry[];
+}
--- src/generator/ng-typescript/ng-typescript.service.ts
+++ src/generator/ng-typescript/ng-typescript.service.ts
@@ -1,7 +1,9 @@
 import kebabCase from 'just-kebab-case';
 import { IDocument } from '../../core/entities/document.model';
 import { ResolveFn } from '../../core/entities/model.model';
+import { generateModelName } from '../../core/utils';
 import { IGenerator, IGeneratorFile } from '../generator.model';
+import { INgtsEnum, INgtsEnumEntry } from './ng-typescript.model';
 
 export class NgTypescriptService implements IGenerator {
 	getName(): string {
@@ -16,12 +18,27 @@ export class NgTypescriptService implements IGenerator {
 		const files: IGeneratorFile[] = [];
 
 		for (const e of doc.enums) {
+			const entries: INgtsEnumEntry[] = [];
+
+			for (const enumEntry of e.entries) {
+				const entry: INgtsEnumEntry = {
+					name: enumEntry.name,
+					value: enumEntry.value,
+				};
+
+				entries.push(entry);
+			}
+
+			const model: INgtsEnum = {
+				name: generateModelName(e.name),
+				isStringlyTyped: e.type === 'string',
+				entries,
+			};
+
 			const file: IGeneratorFile = {
 				path: `./enums/${kebabCase(e.name)}.ts`,
 				templateUrl: 'enum',
-				templateData: {
-					data: e,
-				},
+				templateData: { model },
 			};
 
 			files.push(file);
