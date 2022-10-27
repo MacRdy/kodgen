9a43616e8514c39b2932cadd7424e3ec733f77bc
Anton Shibaev <anton@shibaev.io>
2022-10-27 17:25:20 +0300

    minor

--- src/core/renderer/renderer.service.ts
+++ src/core/renderer/renderer.service.ts
@@ -5,7 +5,8 @@ import { TemplateData } from './renderer.model';
 export class RendererService {
 	render(folder: string, name: string, data?: TemplateData): Promise<string> {
 		return new Promise<string>((res, rej) => {
-			const templatePath = path.join(folder, `${name}.ejs`);
+			const templateName = name.endsWith('.ejs') ? name : `${name}.ejs`;
+			const templatePath = path.join(folder, templateName);
 
 			ejs.renderFile(templatePath, data ?? {}, (err, content) => {
 				if (err) {
