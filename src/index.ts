89d54bf302ecbcfdbf3f14d1fe6658409edf30f9
Anton Shibaev <anton@shibaev.io>
2022-11-03 02:00:12 +0300

    fix env

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+export { ArrayModelDef } from './core/entities/schema-entities/array-model-def.model';
+export { EnumDef, EnumEntryDef, EnumType } from './core/entities/schema-entities/enum-def.model';
+export { ObjectModelDef } from './core/entities/schema-entities/model-def.model';
+export {
+	PathDef,
+	PathMethod,
+	PathRequestBody,
+	PathResponse,
+} from './core/entities/schema-entities/path-def.model';
+export { Property } from './core/entities/schema-entities/property.model';
+export { SimpleModelDef } from './core/entities/schema-entities/simple-model-def.model';
