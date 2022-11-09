1375f4a3ceb9cc9cbb8150c02a6b3c0385454ece
Anton Shibaev <macready.dev@gmail.com>
2022-11-10 01:24:23 +0300

    generator complex configuration test

--- src/generators/generator.service.spec.ts
+++ src/generators/generator.service.spec.ts
@@ -31,9 +31,9 @@ describe('generator', () => {
 
 		jest.mocked(generator)?.getName.mockReturnValue('generator-name');
 
-		const result = service.get('generator-name');
+		expect(service.get('generator-name')).toBe(generator);
 
-		expect(result).toBe(generator);
+		expect(() => service.get('some-generator-name-to-throw')).toThrow('Generator not found.');
 	});
 
 	it('should build simple configuration', async () => {
@@ -78,6 +78,70 @@ describe('generator', () => {
 		expect(fsInstanceMock?.loadJson).not.toHaveBeenCalled();
 		expect(fsInstanceMock?.loadJs).not.toHaveBeenCalled();
 		expect(fsInstanceMock?.exists).not.toHaveBeenCalled();
+
+		configGetSpy.mockRestore();
+	});
+
+	it('should build complex configuration', async () => {
+		const configGetSpy = jest.spyOn(Config, 'get');
+
+		configGetSpy.mockReturnValueOnce({
+			generator: '',
+			input: '',
+			output: './output',
+			clean: true,
+			templateDataFile: './template-data.js',
+			templateDir: './custom-templates',
+		});
+
+		const service = new GeneratorService();
+
+		const fsInstanceMock = jest.mocked(fileServiceMock.mock.instances[0]);
+
+		const rendererInstanceMock = jest.mocked(rendererServiceMock.mock.instances[0]);
+		rendererInstanceMock?.getExtension.mockReturnValue('.ext');
+
+		const additionalTemplateData = { var1: 'var1' };
+		fsInstanceMock?.loadJs.mockResolvedValueOnce(additionalTemplateData);
+
+		pathMock.join.mockReturnValueOnce('./custom-templates/template.ext');
+
+		fsInstanceMock?.exists.mockReturnValueOnce(true);
+
+		rendererInstanceMock?.render.mockResolvedValue('rendered template');
+
+		pathMock.join.mockReturnValueOnce('./output/file');
+
+		const files: IGeneratorFile[] = [
+			{
+				path: './file',
+				template: 'template',
+			},
+		];
+
+		await service.build('./templates', files);
+
+		expect(fsInstanceMock?.removeDirectory).toHaveBeenCalledTimes(1);
+		expect(fsInstanceMock?.removeDirectory).toHaveBeenCalledWith('./output');
+
+		expect(fsInstanceMock?.loadJs).toHaveBeenCalledTimes(1);
+		expect(fsInstanceMock?.loadJs).toHaveBeenCalledWith('./template-data.js');
+
+		expect(fsInstanceMock?.exists).toHaveBeenCalledTimes(1);
+		expect(fsInstanceMock?.exists).toHaveBeenCalledWith('./custom-templates/template.ext');
+
+		expect(pathMock.join).nthCalledWith(1, './custom-templates', 'template.ext');
+		expect(pathMock.join).nthCalledWith(2, './output', './file');
+		expect(pathMock.join).toBeCalledTimes(2);
+
+		expect(rendererInstanceMock?.render).lastCalledWith(
+			'./custom-templates/template.ext',
+			additionalTemplateData,
+		);
+
+		expect(fsInstanceMock?.createFile).lastCalledWith('./output/file', 'rendered template');
+
+		expect(fsInstanceMock?.loadJson).not.toHaveBeenCalled();
 
 		configGetSpy.mockRestore();
 	});
