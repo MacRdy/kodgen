32471599db2517f4cc4af18551b38fd6211ab8ec
Anton Shibaev <macready.dev@gmail.com>
2022-11-19 17:55:55 +0300

    naming spec

--- src/generators/typescript/typescript-generator-naming.service.spec.ts
+++ src/generators/typescript/typescript-generator-naming.service.spec.ts
@@ -1,4 +1,10 @@
 import { ObjectModelDef } from '@core/entities/schema-entities/object-model-def.model';
+import {
+	BODY_OBJECT_ORIGIN,
+	PATH_PARAMETERS_OBJECT_ORIGIN,
+	QUERY_PARAMETERS_OBJECT_ORIGIN,
+	RESPONSE_OBJECT_ORIGIN,
+} from '@core/entities/schema-entities/path-def.model';
 import { TypescriptGeneratorNamingService } from './typescript-generator-naming.service';
 import { TypescriptGeneratorStorageService } from './typescript-generator-storage.service';
 import { generateEntityName } from './typescript-generator.model';
@@ -23,14 +29,38 @@ describe('typescript-generator-naming', () => {
 		const storage = new TypescriptGeneratorStorageService();
 		const service = new TypescriptGeneratorNamingService(storage);
 
-		const entity = new ObjectModelDef('Test');
-
 		const storageMock = jest.mocked(storage);
 		storageMock.getSummary.mockReturnValueOnce([]);
 		storageMock.getSummary.mockReturnValueOnce([{ name: 'Test' }]);
 		storageMock.getSummary.mockReturnValueOnce([{ name: 'Test' }]);
 
+		const entity = new ObjectModelDef('Test');
+
 		expect(service.generateReferenceEntityName(entity)).toStrictEqual('Test');
 		expect(service.generateReferenceEntityName(entity)).toStrictEqual('Test1');
+	});
+
+	it('should generate correct name by origin', () => {
+		const storage = new TypescriptGeneratorStorageService();
+		const service = new TypescriptGeneratorNamingService(storage);
+
+		const storageMock = jest.mocked(storage);
+		storageMock.getSummary.mockReturnValue([]);
+
+		const entity = new ObjectModelDef('Test');
+
+		expect(service.generateReferenceEntityName(entity)).toStrictEqual('Test');
+
+		entity.setOrigin(PATH_PARAMETERS_OBJECT_ORIGIN, true);
+		expect(service.generateReferenceEntityName(entity)).toStrictEqual('Test Path Parameters');
+
+		entity.setOrigin(QUERY_PARAMETERS_OBJECT_ORIGIN, true);
+		expect(service.generateReferenceEntityName(entity)).toStrictEqual('Test Query Parameters');
+
+		entity.setOrigin(BODY_OBJECT_ORIGIN, true);
+		expect(service.generateReferenceEntityName(entity)).toStrictEqual('Test Body');
+
+		entity.setOrigin(RESPONSE_OBJECT_ORIGIN, true);
+		expect(service.generateReferenceEntityName(entity)).toStrictEqual('Test Response');
 	});
 });
