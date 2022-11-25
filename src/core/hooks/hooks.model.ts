1ee760ee5d6679b6c2d34058784d892e3b6ec1d5
Anton Shibaev <macready.dev@gmail.com>
2022-11-25 22:55:58 +0300

    fix typing

--- src/core/hooks/hooks.model.ts
+++ src/core/hooks/hooks.model.ts
@@ -1,2 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type HookFn = (...args: any[]) => any;
+export type AnyFn = (...args: any[]) => any;
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type HookFn = <T extends AnyFn>(defaultFn: T, ...args: any[]) => any;
