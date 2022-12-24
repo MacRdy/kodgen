f1e005bda7124bb8edcb11ceefd5d63c896f0e18
Anton Shibaev <macready.dev@gmail.com>
2022-12-24 18:12:21 +0300

    tests

---
+++ src/core/parser/parser.service.spec.ts
@@ -0,0 +1,10 @@
+jest.mock('js-yaml');
+jest.mock('@apidevtools/swagger-parser');
+
+jest.mock('./v2/v2-parser.service');
+jest.mock('./v3/v3-parser.service');
+jest.mock('./v31/v31-parser.service');
+
+describe('parser', () => {
+	// TODO add tests
+});
