51ec36e4986aa43a1be3da57045a90a4f8b32bd7
Anton Shibaev <macready.dev@gmail.com>
2022-12-25 20:44:49 +0300

    printer test

--- src/core/printer/printer.spec.ts
+++ src/core/printer/printer.spec.ts
@@ -1,14 +1,33 @@
 import { EOL } from 'os';
+import { Config } from '../config/config';
 import { Printer } from './printer';
 
 describe('printer', () => {
 	it('should call process.stdout.write', () => {
-		const spy = jest.spyOn(process.stdout, 'write').mockReturnValue(true);
+		const writeSpy = jest.spyOn(process.stdout, 'write').mockReturnValue(true);
 
 		Printer.info('message');
 
-		expect(spy).toHaveBeenCalledWith('message' + EOL);
+		expect(writeSpy).toHaveBeenCalledWith('message' + EOL);
 
-		spy.mockRestore();
+		writeSpy.mockRestore();
+	});
+
+	it('should call process.stdout.write only with verbose=true', () => {
+		const writeSpy = jest.spyOn(process.stdout, 'write').mockReturnValue(true);
+
+		const configGetSpy = jest.spyOn(Config, 'get');
+
+		configGetSpy.mockReturnValueOnce({ generator: '', input: '', output: '', verbose: false });
+		configGetSpy.mockReturnValueOnce({ generator: '', input: '', output: '', verbose: true });
+
+		Printer.verbose('message');
+		expect(writeSpy).not.toBeCalled();
+
+		Printer.verbose('message');
+		expect(writeSpy).toBeCalledTimes(1);
+
+		writeSpy.mockRestore();
+		configGetSpy.mockRestore();
 	});
 });
