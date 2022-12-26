168db0c3a90a18705bbae75004caff9454ca2f64
Anton Shibaev <macready.dev@gmail.com>
2022-12-26 23:32:56 +0300

    fix tests

--- src/core/parser/parser.service.spec.ts
+++ src/core/parser/parser.service.spec.ts
@@ -1,19 +1,14 @@
 import { dereference } from '@apidevtools/swagger-parser';
-import { load } from 'js-yaml';
 import { ParserService } from './parser.service';
 import { V2ParserService } from './v2/v2-parser.service';
 import { V3ParserService } from './v3/v3-parser.service';
 import { V31ParserService } from './v31/v31-parser.service';
 
 jest.mock('@apidevtools/swagger-parser');
-jest.mock('js-yaml');
 
 jest.mock('./v2/v2-parser.service');
 jest.mock('./v3/v3-parser.service');
 jest.mock('./v31/v31-parser.service');
-
-const jsYamlLoadMock = jest.mocked(load);
-const swaggerParserDereferenceMock = jest.mocked(dereference);
 
 const v2ParserServiceMock = jest.mocked(V2ParserService);
 const v3ParserServiceMock = jest.mocked(V3ParserService);
@@ -21,9 +16,6 @@ const v31ParserServiceMock = jest.mocked(V31ParserService);
 
 describe('parser', () => {
 	beforeEach(() => {
-		jsYamlLoadMock.mockClear();
-		swaggerParserDereferenceMock.mockClear();
-
 		v2ParserServiceMock.mockClear();
 		v3ParserServiceMock.mockClear();
 		v31ParserServiceMock.mockClear();
