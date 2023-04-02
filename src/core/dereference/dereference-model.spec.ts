e85975ffa761c473ebad32ee4159a0a37370bc0d
Anton Shibaev <macready.dev@gmail.com>
2023-04-03 00:57:25 +0300

    new spec

--- src/core/dereference/dereference-model.spec.ts
+++ src/core/dereference/dereference-model.spec.ts
@@ -1,5 +1,16 @@
+import {
+	DEREFERENCE_RESOLVED_VALUE,
+	getDereferenceResolvedValueOrDefault,
+} from './dereference.model';
+
 describe('dereference-model', () => {
 	it('should get resolved value if exists', () => {
-		// TODO getDereferenceResolvedValueOrDefault
+		const value = {};
+
+		const objWithValue = { [DEREFERENCE_RESOLVED_VALUE]: value };
+		const objWithoutValue = {};
+
+		expect(getDereferenceResolvedValueOrDefault(objWithValue)).toBe(value);
+		expect(getDereferenceResolvedValueOrDefault(objWithoutValue)).toBe(objWithoutValue);
 	});
 });
